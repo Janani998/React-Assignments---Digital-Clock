6af802d9cb9bd397a0549de0c8e9892472574e09
Janani998 <68743120+Janani998@users.noreply.github.com>
2020-10-30 22:36:55 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -14,27 +14,30 @@ class App extends Component {
     };
   }
   componentDidMount() {
-    this.timer = setInterval(() => {
-      this.setState({
-        time: new Date().toLocaleTimeString("en-US", {
-          hour: "numeric",
-          minute: "numeric",
-          second: "numeric",
-          hour12: true
-        })
-      });
-    }, 1000);
+    this.timer = setInterval(() => this.tick(), 1000);
   }
 
   componentWillUnMount() {
     clearInterval(this.timer);
   }
 
+  tick() {
+    this.setState({
+      time: new Date().toLocaleTimeString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true
+      })
+    });
+  }
   render() {
     return (
-      <div className="Clock">
-        <h3 id="time">{this.state.time} </h3>
-      </div>
+      <>
+        <div className="Clock">
+          <h3 id="time">{this.state.time} </h3>
+        </div>
+      </>
     );
   }
 }
